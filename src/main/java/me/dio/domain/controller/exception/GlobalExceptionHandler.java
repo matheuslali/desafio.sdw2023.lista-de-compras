e075e849cbef7eaba655c75f8200ffeaa4f65e46
matheuslali <matheus.lali@hotmail.com>
2023-10-21 21:57:02 -0400

    Add application-dev.yml and GlobalExceptionHandler

---
+++ src/main/java/me/dio/domain/controller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,27 @@
+package me.dio.domain.controller.exception;
+
+import me.dio.service.exception.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    public ResponseEntity<String> HandleNoContentException(){
+        return new ResponseEntity<>("Resource ID not found", HttpStatus.NOT_FOUND);
+    }
+
+    public ResponseEntity<String> HandleBusinessException(BusinessException ex){
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    public ResponseEntity<String> handleUnexpectedException (Throwable unexpectedException){
+        var message = "Unexpected server error";
+        LOGGER.error(message, unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
